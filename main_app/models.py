b0db841e4a4231b03f12c2824b44467b2be6a533
Yevgen Bilko <bilko.yevgen@gmail.com>
2023-12-05 00:43:33 +0200

    Change product model verbose names. Add requirements.txt

--- main_app/models.py
+++ main_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class ProductCategory(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, verbose_name="Назва")
 
     def __str__(self):
         return self.name
@@ -10,7 +10,7 @@ class ProductCategory(models.Model):
 
 class ProductImage(models.Model):
     product = models.ForeignKey('Product', related_name='images', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='product_photos/')
+    image = models.ImageField(upload_to='product_photos/', verbose_name="Фото")
 
 
 class SizeQuantity(models.Model):
@@ -22,8 +22,8 @@ class SizeQuantity(models.Model):
         ('L', 'Large'),
         ('XL', 'Extra Large'),
     ]
-    size = models.CharField(max_length=2, choices=size_choices)
-    quantity = models.PositiveIntegerField(default=0)
+    size = models.CharField(max_length=2, choices=size_choices, verbose_name="Розмір")
+    quantity = models.PositiveIntegerField(default=0, verbose_name="Кількість")
 
     def __str__(self):
         return f"{self.get_size_display()} - {self.quantity} available"
--- mich_team_python/urls.py
+++ mich_team_python/urls.py
@@ -21,4 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('main_app.api.urls')),
     path('', include('main_app.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls.jwt'))
 ]

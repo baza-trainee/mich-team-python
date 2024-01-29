c5f10fa4cbd82dbd879ad7229c44deacf05e653b
BilkoYevgen <113464448+BilkoYevgen@users.noreply.github.com>
2024-01-29 15:47:28 +0200

    Update urls.py

--- mich_team_python/urls.py
+++ mich_team_python/urls.py
@@ -33,6 +33,6 @@ urlpatterns = [
     path('', include('cart.api.urls')),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += staticfiles_urlpatterns()
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+

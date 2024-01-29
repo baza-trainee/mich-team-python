ac8bf4f32b044a43f5f19095b074c747d479d189
BilkoYevgen <113464448+BilkoYevgen@users.noreply.github.com>
2024-01-29 15:49:56 +0200

    Update urls.py

--- mich_team_python/urls.py
+++ mich_team_python/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
